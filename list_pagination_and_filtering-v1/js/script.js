aede1a3988cf0e482b939c1bc5af0205c483836a
Mario Almanza <mario_almanza@yahoo.com>
2019-09-12 15:18:38 -0400

    Updated comments for JS

--- list_pagination_and_filtering-v1/js/script.js
+++ list_pagination_and_filtering-v1/js/script.js
@@ -1,46 +1,16 @@
-/******************************************
-Treehouse Techdegree:
-FSJS project 2 - List Filter and Pagination
-******************************************/
-   
-// Study guide for this project - https://drive.google.com/file/d/1OD1diUsTMdpfMDv677TfL1xO2CEkykSz/view?usp=sharing
-
-
-/*** 
-   Add your global variables that store the DOM elements you will 
-   need to reference and/or manipulate. 
-   
-   But be mindful of which variables should be global and which 
-   should be locally scoped to one of the two main functions you're 
-   going to create. A good general rule of thumb is if the variable 
-   will only be used inside of a function, then it can be locally 
-   scoped to that function.
-***/
-const listItem = document.getElementsByClassName('student-item')
+//Gloabal variables that stores DOM element
+const listItem = document.getElementsByClassName('student-item');
 const studentsPerPage = 10;
 
-
-
-
-/*** 
-   Create the `showPage` function to hide all of the items in the 
-   list except for the ten you want to show.
-
-   Pro Tips: 
-     - Keep in mind that with a list of 54 students, the last page 
-       will only display four.
-     - Remember that the first student has an index of 0.
-     - Remember that a function `parameter` goes in the parens when 
-       you initially define the function, and it acts as a variable 
-       or a placeholder to represent the actual function `argument` 
-       that will be passed into the parens later when you call or 
-       "invoke" the function 
-***/
+//created a showPage function that hides all students except the 10 studentson each page
 function showPage(list, page) {
+   //Created two variables to store the start index and the end index 
    const startIndex = (page * studentsPerPage) - studentsPerPage;
    const endIndex = (page * studentsPerPage) - 1;
 
+   //loops over the list parameters
    for(let i = 0; i < list.length; i += 1) {
+      //displays any list item with an index that is >= to start index var and <= end index var
       if ((i >= startIndex) && (i <= endIndex)) {
          list[i].style.display = '';
       }
@@ -52,11 +22,7 @@ function showPage(list, page) {
    showPage(listItem, 1);
 
 
-/*** 
-   Create the `appendPageLinks function` to generate, append, and add 
-   functionality to the pagination buttons.
-***/
-
+//created appendPageLinks function
 function appendPageLinks(list) {
    const numberOfPages = Math.round((list.length / studentsPerPage) + 1);
    const container = document.querySelector('.page');
@@ -76,6 +42,7 @@ function appendPageLinks(list) {
             a.className = 'active';
          }
 
+         // click event listener for each A element
          li.addEventListener('click', (event) => {
             const clickLinks = document.querySelectorAll('a');
             const isClicked = event.target;
@@ -95,5 +62,3 @@ function appendPageLinks(list) {
 
 appendPageLinks(listItem);
 
-
-// Remember to delete the comments that came with this file, and replace them with your own code comments.
